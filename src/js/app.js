32936ea69a0cd1d87ca53a9e79188514db544d9f
cat <colacat1927@gmail.com>
2023-08-09 15:01:16 +0300

    добавил часть попап скрипта

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// import fancybox from "./modules/fancybox.js";
+import fancybox from "./modules/fancybox.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const contacts = document.querySelector(".header__wrapper-contacts");
 	const overlay = document.querySelector(".overlay");
 	const header = document.querySelector(".header");
-// 	const intro = document.querySelector(".intro");
 
 	const navLinks = document.querySelectorAll('.nav__link');
 
@@ -30,9 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		try {
 			if (JSON.parse(burger.getAttribute("aria-expanded"))) {
 				overlay.classList.add("active");
-				// if (intro) {
-					// intro.classList.add("active");
-				// }
 				burger.classList.add("active");
 				wrapper.classList.add("active");
 				nav.classList.add("active");
@@ -41,10 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
 				document.body.style.overflow = "hidden";
 			} else {
                 overlay.classList.remove("active");
-				// if (intro) {
-                    // intro.classList.remove("active");
-                    // }
-                    
                 burger.classList.remove("active");
                 wrapper.classList.remove("active");
                 nav.classList.remove("active");
@@ -78,121 +70,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	slider();
 
-// 	const questions = () => {
-// 		try {
-// 			const e = document.querySelectorAll(".ques__question");
-// 			e.forEach((e=>{
-// 				e.addEventListener("click", (t=>{
-// 					const c = e.querySelector(".ques__caption")
-// 						, a = e.querySelector(".ques__top");
-// 					e.classList.contains("collapsed") ? (e.classList.remove("active"),
-// 					c.classList.remove("active"),
-// 					a.classList.remove("active")) : (e.classList.add("active"),
-// 					c.classList.add("active"),
-// 					a.classList.add("active"))
-// 				}
-// 				))
-// 			}
-// 			)),
-// 			e[0].click()
-// 		} catch {}
-// 	}
+	const popup = (popupSelector, popupOverlaySelector, popupCloseSelector, timeout) => {
+		try {
+			const popup = document.querySelector(popupSelector);
+			const popupOverlay = document.querySelector(popupOverlaySelector);
+			const popupCloseBtn = document.querySelector(popupCloseSelector)
+	
+			const showPopup = (popup, popupOverlay) => {
+				popup.classList.add("active");
+				popupOverlay.classList.add("active");
+			}
+	
+			const closePopup = (popup, popupOverlay, popupCloseBtn) => {
+				popupCloseBtn.addEventListener('click', () => {
+					popup.classList.remove("active");
+					popupOverlay.classList.remove("active");
+				})
+			}
+	
+			setTimeout(() => {
+				showPopup(popup, popupOverlay);
+				closePopup(popup, popupOverlay, popupCloseBtn);
+			}, timeout);
+		} catch(err) {
+			console.log(err);
+		}
 
-// 	questions();
-
-// 	const calculator = () => {
-// 		try {
-// 			const salary = document.querySelector('.calc__salary');
-		
-// 			let event = new Event("changeValue");
-		
-// 			document.addEventListener("changeValue", (e) => {
-// 				calcSumm();
-// 			});
-	
-// 			calcSumm();
-	
-// 			function calcSumm() {
-// 				const days = document.querySelector('[data-row="days"] .calc__board');
-// 				const hours = document.querySelector('[data-row="hours"] .calc__board');
-	
-	
-// 				const daysValue = days.textContent;
-// 				const hoursValue = hours.textContent * 1000;
-	
-// 				let summ = +daysValue * +hoursValue * 4;
-	
-// 				summ = summ.toLocaleString("ru-RU");
-	
-// 				salary.textContent = "+ " + summ + "₽";
-// 			}
-			
-// 			const changeCalcValues = (parentSelector, initValue = 4, limits = {low: 4, high: 6}) => {
-// 				const wrapper = document.querySelector(parentSelector);
-		
-// 				const plus = wrapper.querySelector('[data-action="plus"]');
-// 				const minus = wrapper.querySelector('[data-action="minus"]');
-				
-// 				const board = wrapper.querySelector('.calc__board');
-			
-// 				const { low, high } = limits;
-		
-// 				board.textContent = initValue;
-		
-// 				if (initValue === low) {
-// 					minus.setAttribute('disabled', true);
-// 				}
-		
-// 				if (initValue === high) {
-// 					plus.setAttribute('disabled', true);
-// 				}
-		
-// 				plus.addEventListener('click', () => {
-// 					if (initValue < high) {
-// 						initValue++;
-// 						board.textContent = initValue;
-		
-// 						if (initValue === high) {
-// 							plus.setAttribute('disabled', true);
-// 						}
-		
-// 						if (initValue > low && minus.getAttribute('disabled')) {
-// 							minus.removeAttribute('disabled');
-// 						}
-	
-// 						document.dispatchEvent(event);
-// 					}
-// 				});
-		
-// 				minus.addEventListener('click', () => {
-// 					if (initValue > low) {
-// 						initValue--;
-// 						board.textContent = initValue;
-		
-// 						if (initValue === low) {
-// 							minus.setAttribute('disabled', true);
-// 						}
-		
-// 						if (initValue < high && plus.getAttribute('disabled')) {
-// 							plus.removeAttribute('disabled');
-// 						}
-	
-// 						document.dispatchEvent(event);
-// 					}
-// 				})
-		
-// 			}
-		
-// 			changeCalcValues('[data-row="days"]', 4, {low: 4, high: 6});
-// 			changeCalcValues('[data-row="hours"]', 6, {low: 6, high: 8});
-// 		} catch {
-
-// 		}
-		
-// 	}
-
-// 	calculator();
-
+	}
+	popup(".popup", ".popup__overlay", ".popup__close", 5000);
 // 	fancybox();
 
 });
