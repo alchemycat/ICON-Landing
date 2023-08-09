8a86fcd87494f80b930afc26e7d6f7d6d5eb5b75
cat <colacat1927@gmail.com>
2023-08-09 15:44:22 +0300

    добавил таймер и куки для попап

--- src/js/app.js
+++ src/js/app.js
@@ -76,9 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
 			const popupOverlay = document.querySelector(popupOverlaySelector);
 			const popupCloseBtn = document.querySelector(popupCloseSelector)
 	
+			
 			const showPopup = (popup, popupOverlay) => {
-				popup.classList.add("active");
-				popupOverlay.classList.add("active");
+				if (!getCookie("popup")) {
+					console.log(getCookie("popup"));
+					popup.classList.add("active");
+					popupOverlay.classList.add("active");
+					setCookie('popup', 'true', {secure: true, 'max-age': 600});
+				}
 			}
 	
 			const closePopup = (popup, popupOverlay, popupCloseBtn) => {
@@ -92,12 +97,75 @@ document.addEventListener("DOMContentLoaded", () => {
 				showPopup(popup, popupOverlay);
 				closePopup(popup, popupOverlay, popupCloseBtn);
 			}, timeout);
+
+			function setCookie(name, value, options = {}) {
+
+				options = {
+				  path: '/',
+				  // при необходимости добавьте другие значения по умолчанию
+				  ...options
+				};
+			  
+				if (options.expires instanceof Date) {
+				  options.expires = options.expires.toUTCString();
+				}
+			  
+				let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+			  
+				for (let optionKey in options) {
+				  updatedCookie += "; " + optionKey;
+				  let optionValue = options[optionKey];
+				  if (optionValue !== true) {
+					updatedCookie += "=" + optionValue;
+				  }
+				}
+			  
+				document.cookie = updatedCookie;
+			  }
+
+			  function getCookie(name) {
+				let matches = document.cookie.match(new RegExp(
+				  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+				));
+				return matches ? decodeURIComponent(matches[1]) : undefined;
+			  }
 		} catch(err) {
 			console.log(err);
 		}
 
 	}
-	popup(".popup", ".popup__overlay", ".popup__close", 5000);
+
+	const timer = () => {
+		function updateCountdown(endTime) {
+			const now = new Date().getTime();
+			const timeRemaining = endTime - now;
+		
+			if (timeRemaining <= 0) {
+				clearInterval(countdownInterval);
+				document.querySelector(".hours").textContent = "00";
+				document.querySelector(".minutes").textContent = "00";
+				document.querySelector(".seconds").textContent = "00";
+			} else {
+				const hours = String(Math.floor(timeRemaining / (1000 * 60 * 60))).padStart(2, '0');
+				const minutes = String(Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+				const seconds = String(Math.floor((timeRemaining % (1000 * 60)) / 1000)).padStart(2, '0');
+			
+				document.querySelector(".hours").textContent = hours;
+				document.querySelector(".minutes").textContent = minutes;
+				document.querySelector(".seconds").textContent = seconds;
+			}
+		}
+	
+		const endTime = new Date().getTime() + (5 * 60 * 60 * 1000) + (17 * 60 * 1000) + (15 * 1000);
+
+		const countdownInterval = setInterval(() => updateCountdown(endTime), 1000);
+		
+		updateCountdown(endTime);
+  
+	}
+
+	popup(".popup", ".popup__overlay", ".popup__close", 15000);
+	timer();
 // 	fancybox();
 
 });
